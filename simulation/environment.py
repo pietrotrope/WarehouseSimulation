af6c434b29cd99ae34beed0581f1fe3c3b3d8869
aXhyra <xhyra.alind@gmail.com>
2021-05-17 17:41:31 +0200

    fixed graph generation.

--- simulation/environment.py
+++ simulation/environment.py
@@ -10,7 +10,8 @@ class Environment:
         self.raster_map = None
         self.map_shape = None
         self.graph = None
-        self.graph_to_raster = {}
+        self.raster_to_graph = {}
+        self.__id = 0
         self.__load_map(map_path)
         self.__gen_graph()
 
@@ -37,26 +38,55 @@ class Environment:
         self.map_shape = self.raster_map.shape
 
     def __gen_graph(self):
-        graph_nodes = self.map_shape[0] *\
-                      self.map_shape[1] -\
-                      np.count_nonzero(self.raster_map == 4) +\
-                      self.get_picking_stations_number()
+        graph_nodes = self.map_shape[0] * \
+                      self.map_shape[1] - \
+                      (np.count_nonzero(self.raster_map == 4) -
+                       self.get_picking_stations_number())
         self.graph = Graph(graph_nodes)
 
+        picking_station = (0, 0)
+
+        count = -1
+
+        for i in range(self.map_shape[0]):
+            for j in range(self.map_shape[1]):
+                if self.raster_map[i][j] == 4:
+                    if self.raster_map[i - 1][j] == self.raster_map[i][j - 1] == 0:
+                        count += 1
+                        node = self.graph.get_node(count)
+                        node.coord = (i, j)
+                        node.type = Tile(self.raster_map[i][j])
+                    self.raster_to_graph[(i, j)] = count
+                else:
+                    count += 1
+                    node = self.graph.get_node(count)
+                    node.coord = (i, j)
+                    node.type = Tile(self.raster_map[i][j])
+                    self.raster_to_graph[(i, j)] = count
+
+        for i in range(self.map_shape[0]):
+            for j in range(self.map_shape[1]):
+                node = self.raster_to_graph[(i, j)]
+                if j != 0:
+                    self.graph.add_edge(node, self.raster_to_graph[(i, j - 1)])
+                if i != 0:
+                    self.graph.add_edge(node, self.raster_to_graph[(i - 1, j)])
+
+        '''
         i = j = 0
         picking_station = (0, 0)
 
         for true_i in range(self.map_shape[0]):
-            for true_j in range(self.map_shape[1]):
+            for true_j, node in range(self.map_shape[1]), self.graph.nodes:
                 if self.raster_map[true_i][true_j] == 4:
                     # Found a picking station
                     if self.raster_map[true_i - 1][true_j] == self.raster_map[true_i][true_j - 1] == 0:
+                        count += 1
                         # If this is the first block of the picking station
-                        picking_station = (true_i, true_j)
-                        picking_station_g = (i, j)
+                        picking_station = (i, j)
                         node = self.graph.get_node(self.raster_to_key(picking_station))
                         node.change_type(Tile(self.raster_map[true_i][true_j]))
-                        self.__add_edge((true_i, true_j), (true_i, true_j - 1))
+                        self.__add_edge(picking_station, (i, j - 1))
 
                         picking_station_blocks = [picking_station]
 
@@ -70,7 +100,6 @@ class Environment:
                                 self.__add_edge(picking_station, (picking_station[0] - 1, j))
                                 if self.raster_map[tmp_i][tmp_j + 1] == 0:
                                     self.__add_edge(picking_station, (picking_station[0], j))
-
                                 if self.raster_map[tmp_i - 1][tmp_j] == -1:
                                     self.__add_edge(picking_station, (picking_station[0] + 1, j))
                                     if self.raster_map[tmp_i][tmp_j + 1] == 0:
@@ -82,10 +111,15 @@ class Environment:
                             tmp_j = true_j
 
                         self.graph_to_raster[self.raster_to_key((i, j))] = picking_station_blocks
-                elif self.raster_map[true_i][true_j] == -1:
-                    if self.raster_map[true_i][true_j + 1] == 0:
                         j = (j + 1) % self.map_shape[1]
+                elif self.raster_map[true_i][true_j] == -1:
+                    pass
                 else:
+                    count += 1
+                    print('{}: {}, {}, {}'.format(self.raster_to_key((i, j)),
+                                                  [(i, j)],
+                                                  [(true_i, true_j)],
+                                                  Tile(self.raster_map[true_i][true_j])))
                     self.graph_to_raster[self.raster_to_key((i, j))] = [(true_i, true_j)]
                     node = self.graph.get_node(self.raster_to_key((i, j)))
                     node.change_type(Tile(self.raster_map[true_i][true_j]))
@@ -96,7 +130,8 @@ class Environment:
                             self.__add_edge((i, j), (i, j - 1))
                         if i != 0:
                             self.__add_edge((i, j), (i - 1, j))
-                if self.raster_map[true_i][true_j] != 4 and self.raster_map[true_i][true_j] != -1:
+
                     j = (j + 1) % self.map_shape[1]
-            i += 1
-        self.raster_map[self.raster_map == -1] = 4
+            if j == 0:
+                i += 1
+        self.raster_map[self.raster_map == -1] = 4'''
