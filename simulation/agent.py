a27b5f52e704f15f38fcab919f6dcd4a339204ae
Alind Xhyra <xhyra.alind@gmail.com>
2021-05-19 17:43:16 +0200

    added basic Agent class implementation

--- simulation/agent.py
+++ simulation/agent.py
@@ -1,4 +1,13 @@
-class Agent:
+import json
+import threading
 
-    def __init__(self):
-        pass
+
+class Agent(threading.Thread):
+
+    def __init__(self, id, position):
+        super().__init__()
+        self.id = id
+        self.position = position
+        with open('astar/astarRoutes.json', 'r') as f:
+            self.routes = json.load(f)
+
