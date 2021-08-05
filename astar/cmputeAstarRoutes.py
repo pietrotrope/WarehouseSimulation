149e67efd690a678a53f59717f3d3cc86ee7c779
pietrotrope <pi.tropeano@gmail.com>
2021-08-05 17:03:13 +0200

    Updated astar

--- astar/cmputeAstarRoutes.py
+++ astar/cmputeAstarRoutes.py
@@ -3,12 +3,14 @@ from astar.astar import computeAstarRoutes
 import time
 import json
 
-
 def AstarRoutesToFile():
     env = Environment()
     start = time.time()
     dic = computeAstarRoutes(env)
+    for key in dic:
+        dic[key] = dict(dic[key])
     with open("astarRoutes.json", "w") as fp:
         json.dump(dic, fp)
-    print(time.time() - start)
+    print(time.time()-start)
     print("seconds to compute all the routes")
+    
